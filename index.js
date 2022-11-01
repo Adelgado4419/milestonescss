a8d568f7d8dad6a20800a49f97ecba618323effa
Alexis Delgado <alexisdelgado@MacBook-Pro-7.local>
2022-10-31 19:22:48 -0700

    GOT TIMER WORKING

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
+const timeH = document.getElementById("countdown")
+let timeSecond = 10;
 
-const startingSeconds = 60;
-let time = startingSeconds;
+displayTime(timeSecond)
 
-const countdownTimer = document.getElementById('seconds-countdown');
+const countDown = setInterval(()=>{
+    timeSecond--;
+    displayTime(timeSecond);
+    if(timeSecond <=0 || timeSecond<1){
+        endTime();
+        clearInterval(countDown)
+    }
+},1000)
 
-setInterval(updateCountdown, 1000);
+function displayTime(second){
+    const min = Math.floor(second / 60);
+    const sec = Math.floor(second % 60);
+    timeH.innerHTML = `${sec <10?'0':''} ${sec}`
+}
 
-function updateCountdown() {
-    const time = Math.floor(time)
-    let seconds = time;
-    
-
-    countdownTimer.innerHTML = `${seconds}`;
-    time--;
+function endTime(){
+    timeH.innerHTML = "YOURE FINISHED!"
 }
