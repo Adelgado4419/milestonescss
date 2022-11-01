64e915f8c7c7e709295b56a1316616fe4f50efbc
Alexis Delgado <alexisdelgado@MacBook-Pro-7.local>
2022-10-31 18:43:38 -0700

    removed capital D from ID

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-//counter code
-const startingMinutes = 60;
-let time = startingMinutes;
 
-const countdownTimer = document.getElementByID('seconds-countdown');
+const startingSeconds = 60;
+let time = startingSeconds;
 
-setInterval(updateCountdowveronn, 1000);
+const countdownTimer = document.getElementById('seconds-countdown');
+
+setInterval(updateCountdown, 1000);
 
 function updateCountdown() {
     const time = Math.floor(time)
     let seconds = time;
+    
 
     countdownTimer.innerHTML = `${seconds}`;
     time--;
