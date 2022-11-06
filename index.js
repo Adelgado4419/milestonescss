df5917d63e32e739ad32f78efbc942cd18d96326
Alexis Delgado <alexisdelgado@MacBook-Pro-7.local>
2022-11-06 13:18:07 -0800

    added in back to top feature

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//START TIMER
+//START TIMER CODE!!
 const timeH = document.getElementById("countdown")
 const youreFinished = document.getElementById("questionformform")
 let timeSecond = 50;
@@ -21,79 +21,34 @@ function displayTime(second){
 }
 
 function endTime(){
-    // timeH.innerHTML = "YOURE FINISHED!"//not sure if I want this
     youreFinished.innerHTML = "YOU HAVE FAILED ME FOR THE LAST TIME"
 }
 // END TIMER
 
+//counter eval code
 
-//points keeper
-// function getResults() {
+function getTotal(){         
+    var totalCorrect = 0;          
+        for(var a = 1; a <= 9; a++) {
+            var radios = document.getElementsByName('question'+a);
+            for(var x = 0; x < radios.length; x++) {
+        var radio = radios[x];
+        if(radio.value == "correct" && radio.checked) {
+            totalCorrect++;
+        }
+    }
+}                   
+    document.getElementById("questionformform").innerHTML = 
+    "Correct Answers:" + totalCorrect + "/9";
+    clearInterval(countDown)
+}
 
-//     //CORRECT ANSWERS
-//     var amountCorrect = 0;
-  
-//   // LOOP FOR GOING THROUGH ALL QUESTIONS
-//     for(var i = 1; i <= 5; i++) {
-//       var radioName = document.getElementsByName('question'+i);
-  
-//   //LOOP FOR CHECKING ANSWERS INSIDE EACH RADIO
-//       for(var x = 0; x < radioName.length; x++) {
-//         var radioValue = radioName[j];
-//         if(radioValue.value == "correct" && radioValue.checked) {
-//           amountCorrect+= 1;
-//         }
-//       }
-//     }
-//   document.getElementById('results').innerHTML =
-//   "Correct Answers " + amountCorrect + "/9"; 
-  
-//   }
-
-
-document.getElementById("submit").onclick = function() {myFunction()};
-
-function myFunction() {
-  document.getElementById("results").innerHTML = "YOU CLICKED ME!";
-
-
-// let points= 0;
-// function results() {
-//     let points= 0;
-//     if (document.getElementById(answer1).checked) {
-//         points+= 1;
-//         console.log(points)}
-//     if (document.getElementById(answer2).checked) {
-//         points+= 1;
-//         console.log(points)}
-//     if (document.getElementById(answer3).checked) {
-//         points+= 1;
-//         console.log(points)}
-//     if (document.getElementById(answer4).checked) {
-//         points+= 1;
-//         console.log(points)}
-//     if (document.getElementById(answer5).checked) {
-//         points+= 1;
-//         console.log(points)}
-//     if (document.getElementById(answer6).checked) {
-//         points+= 1;
-//         console.log(points)}
-//     if (document.getElementById(answer7).checked) {
-//         points+= 1;
-//         console.log(points)}
-//     if (document.getElementById(answer8).checked) {
-//         points+= 1;
-//         console.log(points)}
-//     if (document.getElementById(answer9).checked) {
-//         points+= 1;
-//         console.log(points)}
-//     }
-
-// results()
-
-//Logic to show score on results page
-
-
-// set value of the question, you can create an event listener as well so 
-//that if anakin1 is not selected, a health is removed
-//for html form, https://www.w3schools.com/html/tryit.asp?filename=tryhtml_form_radio
+function backToTop(){
+    document.documentElement.scrollTop = 0;
+}
+// function visualNotification() {
+//     if (amountCorrect <=6){
+//     document.getElementById("countdown").innerHTML = "You Lost!"
+// } else {
+//     document.getElementById("countdown").innerHTML = "Good job, my apprentice"
+// }
